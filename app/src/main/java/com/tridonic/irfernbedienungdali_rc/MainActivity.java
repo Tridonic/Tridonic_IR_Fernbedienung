3189ca8dde2fa1d8ce8cfe94bf3f74a92a0d64c5
Lozuras <duff.dario@gmail.com>
2015-04-08 14:40:23 +0200

    Installationsmodus angepasst
    Layout angepasst

--- app/src/main/java/com/tridonic/irfernbedienungdali_rc/MainActivity.java
+++ app/src/main/java/com/tridonic/irfernbedienungdali_rc/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.installationsmodus);
+        setContentView(R.layout.programmiermodus);
 
        // spinner = (Spinner) findViewById(R.id.layer_chooser);
        // addLayer();
